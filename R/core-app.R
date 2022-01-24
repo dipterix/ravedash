19b2b923103ac3bb61092cb7a20cfb41902a3f35
dipterix <dipterix.wang@gmail.com>
2022-01-24 11:02:46 -0500

    corrected session functions

--- R/core-app.R
+++ R/core-app.R
@@ -6,10 +6,27 @@ session_root <- function(ensure = FALSE){
   normalizePath(cache_path, mustWork = FALSE)
 }
 
-ensure_template <- function(path){
-  template_path <- file.path(R_user_dir("ravedash", 'data'), 'themes', 'AdminLTE3-rave')
-  if(!dir.exists(template_path)){
-    shidashi::use_template(path = template_path, user = 'dipterix', theme = 'AdminLTE3-rave')
+ensure_template <- function(path, use_cache = TRUE){
+  template_path <- file.path(R_user_dir("ravedash", 'data'), 'themes', 'rave-pipelines')
+  backup_path <- file.path(R_user_dir("ravedash", 'data'), 'backups')
+  backup_template_path <- file.path(backup_path, 'rave-pipelines')
+
+  if(!use_cache || !dir.exists(template_path)){
+    if(dir.exists(template_path)){
+      raveio::dir_create2(backup_path)
+      if(dir.exists(backup_template_path)){
+        unlink(backup_template_path, recursive = TRUE)
+      }
+      file.copy(template_path, backup_path, recursive = TRUE, overwrite = TRUE)
+      unlink(template_path, recursive = TRUE)
+    }
+    shidashi::use_template(path = template_path, user = 'dipterix', theme = 'rave-pipelines', branch = "master")
+    if(dir.exists(backup_template_path)){
+      unlink(backup_template_path, recursive = TRUE)
+      if(!length(list.files(backup_path))){
+        unlink(backup_path, recursive = TRUE)
+      }
+    }
   }
 
   fs <- list.files(template_path, full.names = TRUE)
@@ -20,7 +37,8 @@ ensure_template <- function(path){
   normalizePath(path)
 }
 
-new_session <- function() {
+#' @export
+new_session <- function(update = FALSE) {
 
   # o <- raveio::pipeline_root()
   # on.exit({
@@ -39,14 +57,13 @@ new_session <- function() {
   }
 
   # create a temporary repository
-  rave_id <- paste0(strftime(Sys.time(), "session-%y%m%d-%M%H%S-%Z-"), toupper(rand_string(4)))
+  session_id <- paste0(strftime(Sys.time(), "session-%y%m%d-%M%H%S-%Z-"), toupper(rand_string(4)))
   cache_path <- session_root(ensure = TRUE)
   cache_path <- normalizePath(cache_path)
-
-  app_path <- file.path(cache_path, rave_id)
+  app_path <- file.path(cache_path, session_id)
 
   # raveio::dir_create2(app_path)
-  ensure_template(app_path)
+  ensure_template(app_path, use_cache = !update)
 
   pipeline_path <- file.path(app_path, "_pipelines")
 
@@ -75,25 +92,57 @@ new_session <- function() {
     overwrite = TRUE, copy.date = TRUE
   )
 
-  use_session(rave_id)
+  use_session(session_id)
 
 }
 
-use_session <- function(rave_id) {
+#' @export
+use_session <- function(session_id) {
   cache_path <- session_root()
-  app_path <- file.path(cache_path, rave_id)
+  app_path <- file.path(cache_path, session_id)
   app_path <- normalizePath(app_path, mustWork = TRUE)
 
-  list(
-    launch_app = function(...){
-      shidashi::render(root_path = app_path, ...)
-    }
+  structure(
+    list(
+      launch_app = function(...){
+        shidashi::render(root_path = app_path, ...)
+      },
+      session_id = session_id
+    ),
+    class = "rave-dash-session"
   )
 }
 
-remove_session <- function(rave_id){
-  if(grepl("^rave-[0-9]{6}-[0-9]{6}-[a-zA-Z]+-[A-Z0-9]{4}$", rave_id)){
-    session_path <- file.path(session_root(), rave_id)
+#' @export
+`print.rave-dash-session` <- function(x, ...){
+  vname <- substitute(x)
+  session_id <- x$session_id
+  session_path <- file.path(session_root(), session_id)
+  cat(sprintf("RAVE session <%s>\n", session_id))
+  timestamp <- sub("\\-[a-zA-Z0-9]{4}$", "", session_id)
+  timestamp <- strsplit(timestamp, "-")[[1]]
+  tz <- timestamp[[4]]
+  timestamp <- paste(timestamp[c(2,3)], collapse = "-")
+  timestamp <- strptime(timestamp, "%y%m%d-%H%M%S")
+  timestamp <- strftime(timestamp, usetz = FALSE)
+  timestamp <- paste(timestamp, tz)
+
+  if(dir.exists(session_path)){
+    cat("  Path:", session_path, "\n")
+  } else {
+    cat("  Path:", session_path, "(invalid path)\n")
+  }
+  cat("  Date created:", timestamp, "\n\n")
+
+  vname <- paste(deparse(vname), collapse = "\n")
+  cat(sprintf("Please run `%s$launch_app()` to launch the session.\n", vname))
+
+}
+
+#' @export
+remove_session <- function(session_id){
+  if(grepl("^session-[0-9]{6}-[0-9]{6}-[a-zA-Z]+-[A-Z0-9]{4}$", session_id)){
+    session_path <- file.path(session_root(), session_id)
     if(dir.exists(session_path)){
       unlink(session_path, recursive = TRUE)
       return(invisible(TRUE))
@@ -102,8 +151,9 @@ remove_session <- function(rave_id){
   return(invisible(FALSE))
 }
 
+#' @export
 list_session <- function(path = session_root()){
   dirs <- list.dirs(path = path, full.names = FALSE, recursive = FALSE)
-  sel <- grepl("^rave-[0-9]{6}-[0-9]{6}-[a-zA-Z]+-[A-Z0-9]{4}$", dirs)
-  dirs[sel]
+  sel <- grepl("^session-[0-9]{6}-[0-9]{6}-[a-zA-Z]+-[A-Z0-9]{4}$", dirs)
+  lapply(dirs[sel], use_session)
 }
