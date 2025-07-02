ab4f52247d111e79f7bd9ce7fa92271429e050f2
dipterix <dipterix.wang@gmail.com>
2025-07-02 14:22:01 -0400

    Allows stitching events; 3D viewer loader uses proxy rather than re-rendering the brain

--- R/preset-loader-3dviewer.R
+++ R/preset-loader-3dviewer.R
@@ -35,36 +35,27 @@ presets_loader_3dviewer <- function(
 
     get_subject <- loader_subject$get_tool("get_subject")
 
-    electrode_table <- shiny::bindEvent(
+    local_reactives <- shiny::reactiveValues(
+      brain = NULL
+    )
+
+    brain_proxy <- threeBrain::brain_proxy(outputId = "loader_3d_viewer", session = session)
+
+    electrode_table <- shiny::debounce(shiny::bindEvent(
       shiny::reactive({
-        if(!loader_subject$sv$is_valid()){ return() }
+        if(!loader_subject$sv$is_valid()){ return(NULL) }
         subject <- get_subject()
+
+        all_electrodes <- subject$electrodes
+
+        if(!length(all_electrodes)) { return(NULL) }
 
         subject_code <- subject$subject_code
         project_name <- subject$project_name
         electrodes_text <- loader_electrodes$current_value
         reference_name <- loader_reference$current_value
 
-        brain <- comp$container$get_cache("loader_subject_brain", default = NULL)
-        if(!inherits(brain, "rave-brain") ||
-           !identical(brain$subject_code, subject_code)){
-          logger("Re-generate loader's brain", level = 'trace')
-          brain <- raveio::rave_brain(subject, surfaces = 'pial')
-        } else {
-          logger("Using cached loader's brain", level = 'trace')
-        }
-        comp$container$set_cache(key = "loader_subject_brain",
-                                 value = brain, expire_after = 100)
-        # TODO: brain is null
-        if(is.null(brain)){ return() }
-
         electrodes <- dipsaus::parse_svec(electrodes_text)
-        all_electrodes <- subject$electrodes
-
-
-        if(!length(all_electrodes)) {
-          return(NULL)
-        }
 
         valid_electrodes <- subject$valid_electrodes(reference_name = reference_name)
         val <- rep("Not Loading", length(all_electrodes))
@@ -84,86 +75,133 @@ presets_loader_3dviewer <- function(
       loader_subject$current_value,
       loader_electrodes$current_value,
       loader_reference$current_value,
+      ignoreNULL = FALSE, ignoreInit = FALSE
+    ), millis = 500, domain = session)
+
+    shiny::bindEvent(
+      observe({
+        tbl <- electrode_table()
+        if(!is.data.frame(tbl)) { return() }
+        brain_proxy$set_electrode_data(data = tbl)
+      }),
+      local_reactives$brain,
+      electrode_table(),
+      ignoreNULL = FALSE, ignoreInit = TRUE
+    )
+
+    shiny::bindEvent(
+      observe({
+        theme <- shidashi::get_theme(tools$theme_event)
+        brain_proxy$set_background(theme$background)
+      }),
+      shidashi::get_theme(tools$theme_event),
       ignoreNULL = TRUE, ignoreInit = TRUE
     )
 
-    viewer <-
+
+
+    viewer_output_id <- "loader_3d_viewer"
+    register_output(
       shiny::bindEvent(
-        shiny::bindCache(
+        threeBrain::renderBrain({
 
-          shiny::reactive({
+          shiny::validate(
+            shiny::need(loader_subject$sv$is_valid(), message = "")
+          )
 
-            shiny::invalidateLater(500)
+          subject <- get_subject()
 
-            brain <- comp$container$get_cache("loader_subject_brain", default = NULL)
-            if(!inherits(brain, "rave-brain")){ return() }
+          brain <- raveio::rave_brain(subject, surfaces = 'pial', overlays = NULL, annotations = NULL)
 
-            tbl <- electrode_table()
-            if(is.data.frame(tbl) && nrow(tbl)) {
-              brain$set_electrode_values(tbl)
-            }
+          if(is.null(brain)) {
+            stop("No brain instance")
+          }
 
-            theme <- shidashi::get_theme(tools$theme_event)
-            logger("Re-generate loader's viewer", level = 'trace')
-            wg <- brain$plot(
+          tbl <- electrode_table()
+          if(is.data.frame(tbl) && nrow(tbl)) {
+            brain$set_electrode_values(tbl)
+          }
+
+          theme <- shidashi::get_theme(tools$theme_event)
+
+          logger("Re-generate loader's viewer", level = 'trace')
+
+          new_brain <- shiny::isolate(is.null(local_reactives$brain))
+
+          local_reactives$brain <- brain
+
+          if( new_brain ) {
+            camera_position <- c(0, 0, 500)
+            camera_up <- c(0, 1, 0)
+
+            brain$plot(
               volumes = FALSE,
+              atlases = FALSE,
+              side_canvas = FALSE,
+              control_display = FALSE,
               start_zoom = 1,
+              background = theme$background,
+              palettes = list(Value = c("pink", "orange", "gray80")),
+              controllers = list(
+                "Background Color" = theme$background,
+                "Show Time" = FALSE
+              ),
+              custom_javascript = sprintf(
+                '
+                // Remove the focus box
+                if( canvas.focus_box ) {
+                  canvas.focus_box.visible = false;
+                }
+
+                // set camera
+                canvas.mainCamera.position.set( %f , %f , %f );
+                canvas.mainCamera.up.set( %f , %f , %f );
+                canvas.mainCamera.updateProjectionMatrix();
+
+                // Let shiny know the viewer is ready
+                if( window.Shiny ) {
+                  window.Shiny.setInputValue("%s", "%f");
+                }
+
+                // Force render one frame (update the canvas)
+                canvas.needsUpdate = true;
+                ',
+                camera_position[[1]], camera_position[[2]], camera_position[[3]],
+                camera_up[[1]], camera_up[[2]], camera_up[[3]],
+                session$ns( viewer_output_id ),
+                Sys.time()
+              )
+            )
+          } else {
+
+            brain$render(
+              outputId = viewer_output_id,
+              volumes = FALSE,
               atlases = FALSE,
               side_canvas = FALSE,
               control_display = FALSE,
               # show_modal = TRUE,
-              background = theme$background,
-              palettes = list(Value = c("red", "navy", "gray80")),
+              # start_zoom = 1,
+              # background = theme$background,
+              palettes = list(Value = c("pink", "orange", "gray80")),
               controllers = list(
                 "Background Color" = theme$background,
                 "Show Time" = FALSE
               )
             )
 
-            # comp$container$set_cache("loader_subject_brain_instance", wg, expire_after = Inf)
-            wg
+          }
 
-          }),
-          shidashi::get_theme(tools$theme_event),
-          electrode_table(),
-          cache = "session"
-        ),
-
-        shidashi::get_theme(tools$theme_event),
-        electrode_table(),
-        ignoreNULL = TRUE
-      )
-
-
-    register_output(
-      shiny::bindEvent(
-        threeBrain::renderBrain({
-          wg <- viewer()
-          shiny::validate(shiny::need(!is.null(wg), message = ""))
-          return(wg)
         }),
-        viewer(), ignoreNULL = FALSE, ignoreInit = FALSE
+        loader_project$current_value,
+        loader_subject$current_value,
+        loader_subject$sv$is_valid(),
+        ignoreNULL = FALSE, ignoreInit = FALSE
       ),
-      outputId = "loader_3d_viewer",
+      outputId = viewer_output_id,
       export_type = "3dviewer",
       session = session
     )
-    # output$loader_3d_viewer <- shiny::bindEvent(
-    #   render_output(
-    #     outputId = "loader_3d_viewer",
-    #     .session = session,
-    #     renderer = threeBrain::renderBrain,
-    #     .export_type = "3dviewer",
-    #     .export_name = "electrodes-to-load",
-    #     expr = {
-    #       wg <- viewer()
-    #       shiny::validate(shiny::need(!is.null(wg), message = ""))
-    #
-    #       return(wg)
-    #     }
-    #   ),
-    #   viewer(), ignoreNULL = FALSE, ignoreInit = FALSE
-    # )
 
   }
 
--- R/preset-loader-epoch.R
+++ R/preset-loader-epoch.R
@@ -7,7 +7,8 @@ presets_loader_epoch <- function(
   varname = "epoch_choice",
   label = "Epoch and Trial Duration",
   loader_project_id = "loader_project_name",
-  loader_subject_id = "loader_subject_code"
+  loader_subject_id = "loader_subject_code",
+  allow_stitch = FALSE
 ){
   comp <- RAVEShinyComponent$new(id = id, varname = varname)
   comp$depends <- c(loader_project_id, loader_subject_id)
@@ -19,11 +20,17 @@ presets_loader_epoch <- function(
 
   pre_varname <- comp$get_sub_element_varnames("trial_starts")
   post_varname <- comp$get_sub_element_varnames("trial_ends")
+  pre_event_varname <- comp$get_sub_element_varnames("trial_starts_rel_to_event")
+  post_event_varname <- comp$get_sub_element_varnames("trial_ends_rel_to_event")
+
+  allow_stitch <- isTRUE(as.logical(allow_stitch))
 
   comp$ui_func <- function(id, value, depends){
 
     pre <- comp$get_settings_value(key = pre_varname, default = -1)
     post <- comp$get_settings_value(key = post_varname, default = 2)
+    pre_event <- comp$get_settings_value(key = pre_event_varname, default = "Trial Onset")
+    post_event <- comp$get_settings_value(key = post_event_varname, default = "Trial Onset")
 
     ravedash::flex_group_box(
       title = label,
@@ -45,6 +52,23 @@ presets_loader_epoch <- function(
           value = pre
         )
       ),
+      local({
+        if(allow_stitch) {
+          shiny::tagList(
+            shidashi::flex_item(
+              shiny::selectInput(
+                inputId = comp$get_sub_element_id("trial_starts_rel_to_event", with_namespace = TRUE),
+                label = "anchor to event",
+                choices = unique(c("Trial Onset", pre_event)),
+                selected = pre_event
+              )
+            ),
+            shidashi::flex_break()
+          )
+        } else {
+          NULL
+        }
+      }),
       shidashi::flex_item(
         shiny::numericInput(
           inputId = comp$get_sub_element_id("trial_ends", with_namespace = TRUE),
@@ -53,6 +77,20 @@ presets_loader_epoch <- function(
           value = post
         )
       ),
+      local({
+        if(allow_stitch) {
+          shidashi::flex_item(
+            shiny::selectInput(
+              inputId = comp$get_sub_element_id("trial_ends_rel_to_event", with_namespace = TRUE),
+              label = "anchor to event",
+              choices = unique(c("Trial Onset", post_event)),
+              selected = post_event
+            )
+          )
+        } else {
+          NULL
+        }
+      }),
       shidashi::flex_break(),
       shidashi::flex_item(
         shinyWidgets::prettyCheckbox(
@@ -119,12 +157,55 @@ presets_loader_epoch <- function(
           choices = epoch_choices,
           selected = epoch_name
         )
+
       }),
       loader_project$current_value,
       loader_subject$current_value,
       watch_loader_opened(session = session),
       ignoreNULL = TRUE
     )
+
+    if( allow_stitch ) {
+      shiny::bindEvent(
+        observe({
+
+          subject <- get_subject()
+          if(!inherits(subject, "RAVESubject")) { return() }
+
+          # also get epoch events
+          epoch_name <- comp$get_sub_element_input(NULL)
+          if(!isTRUE(epoch_name %in% subject$epoch_names)) { return() }
+          epoch <- subject$get_epoch(epoch_name = epoch_name)
+          available_events <- epoch$available_events
+          available_events[available_events == ""] <- "Trial Onset"
+          available_events <- unique(c("Trial Onset", available_events))
+
+          start_event <- comp$get_sub_element_input("trial_starts_rel_to_event") %OF% available_events
+
+          shiny::updateSelectInput(
+            session = session,
+            inputId = comp$get_sub_element_id("trial_starts_rel_to_event", with_namespace = FALSE),
+            choices = available_events,
+            selected = start_event
+          )
+
+          end_event <- comp$get_sub_element_input("trial_ends_rel_to_event") %OF% available_events
+
+          shiny::updateSelectInput(
+            session = session,
+            inputId = comp$get_sub_element_id("trial_ends_rel_to_event", with_namespace = FALSE),
+            choices = available_events,
+            selected = end_event
+          )
+
+        }),
+        comp$get_sub_element_input(NULL),
+        loader_project$current_value,
+        loader_subject$current_value,
+        watch_loader_opened(session = session),
+        ignoreNULL = TRUE
+      )
+    }
 
   }
 
